f21a605a243bb3be480a52713dcf905a52205227
larsbeckmusic <larsbeckmusic@gmail.com>
2025-12-31 20:18:23 +0100

    updated router

--- src/router/index.js
+++ src/router/index.js
@@ -43,8 +43,8 @@ const router = createRouter({
  */
 router.afterEach((to) => {
   document.title = to.meta.title
-    ? `${to.meta.title} – DOBA Media`
-    : "DOBA Media";
+    ? `${to.meta.title} – Jens und Lars`
+    : "Jens und Lars";
 });
 
 export default router;
