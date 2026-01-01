1bdcc9e58d01303b19719433b501f6b271691376
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-01 17:05:58 +0100

    updated UI Kit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import ui from '@nuxt/ui/vite'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, fileURLToPath(new URL('.', import.meta.url)), 'VITE_')
@@ -12,6 +13,7 @@ export default defineConfig(({ mode }) => {
       vue(),
       vueJsx(),
       vueDevTools(),
+      ui(),
     ],
 
     resolve: {
